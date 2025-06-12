b1d244e37fdfdaba0a725eb9ac88b7c563d99dec
Farija <farija.wafula@strathmore.edu>
2025-06-12 19:02:06 +0300

    Removed Array.py Character.py Equivalent.py Seconds.py Square.py Volume.py

--- Equivalent.py
+++
@@ -1,6 +0,0 @@
-x = 0
-y = 20
-while y >= 6: #it will be less after the 2 below are performed
-    y -= 4
-    x += 2 / y
-print(f"x = {x:.2f}")
--- Seconds.py
+++
@@ -1,3 +0,0 @@
-days = int(input("Enter number of days:"))
-seconds = days*24*60*60 # 24 hours 60 mins and 60 secs
-print(f"{days}days have{seconds} seconds")
